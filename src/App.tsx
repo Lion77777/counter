2b065b7b28d805c4cee4362effe1847b8c1b01b6
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-03-25 00:09:17 +0400

    Implement counter logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [spanClassName, setSpanClassName] = useState('');
+  const [incBtnClassName, setIncBtnClassName] = useState('');
+  const [rsBtnClassName, setRsBtnClassName] = useState('');
+
+  useEffect(() => {
+    if(count > 4) {
+      setSpanClassName('index-alarm')
+      setIncBtnClassName('inactive-btn')
+      setCount(5);
+    }
+    if(count === 0) {
+      setRsBtnClassName('inactive-btn');
+      setIncBtnClassName('active-btn');
+    } else {
+      setRsBtnClassName('');
+
+    }
+  }, [count])
+
+  function resetBtn() {
+      setCount(0);
+      setSpanClassName('');
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='counter'>
+        <div className='counter__index'>
+          <span className={spanClassName}>{count}</span>
+        </div>
+        <div className='counter__buttons'>
+          <button onClick={() => setCount((count) => count + 1)} className={incBtnClassName}>inc</button>
+          <button onClick={resetBtn} className={rsBtnClassName}>reset</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
